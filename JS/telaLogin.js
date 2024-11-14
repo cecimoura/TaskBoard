fd5873e3e8a6b056dac8680e695eda27d5e6d08d
cecimoura <cecimoura91@gmail.com>
2024-11-13 21:13:31 -0300

    Tela login

---
+++ JS/telaLogin.js
@@ -0,0 +1,10 @@
+let buttonAccess = document.getElementById("acessar");
+
+buttonAcess.addEventListener("click",(e)=>{
+    e.preventDefault();
+   
+   let email = document.getElementById("email")
+   
+    console.log("botão clicado");
+})
+
