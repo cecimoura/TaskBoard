f1778dfc979abcc6790a0223a6b9e3b5eaa9142d
cecimoura <cecimoura91@gmail.com>
2024-11-25 13:10:50 -0300

    dark mode

--- TaskBoard/scripts/pages/index.js
+++ TaskBoard/scripts/pages/index.js
@@ -19,15 +19,17 @@ function myFunction() {
   }
 
 
-  //modo escuro
+//modo escuro
 let containerSwitch = document.getElementById('container-switch');
-let body = document.querySelector('body')
-let cabecalho = document.getElementById('cabecalho')
-let containerBotoes = document.getElementById('container-botoes')
+let body = document.querySelector('body');
+let cabecalho = document.getElementById('cabecalho');
+let containerBotoes = document.getElementById('container-botoes');
+
 
 containerSwitch.addEventListener('click',()=>{
     containerSwitch.classList.toggle('dark')
     body.classList.toggle('dark')
     cabecalho.classList.toggle('dark')
     containerBotoes.classList.toggle('dark')
+    logo.classList.toggle('dark')
 })
