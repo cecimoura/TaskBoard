4abf0547dfcedd587f50f2ac62c655013c815271
cecimoura <cecimoura91@gmail.com>
2024-12-02 12:06:31 -0300

    alterando o modo dark

--- TaskBoard/scripts/pages/darkMode.js
+++ TaskBoard/scripts/pages/darkMode.js
@@ -1,17 +1,30 @@
+import { API_BASE_URL } from "../../config/apiConfig.js";
+import { saveToLocalStorage } from "../utils/storage.js";
+
+
 //modo escuro
 let containerSwitch = document.getElementById('container-switch');
 let body = document.querySelector('body');
 let cabecalho = document.getElementById('cabecalho');
 let containerBotoes = document.getElementById('container-botoes');
 
-function clickTema() {
+async function clickTema() {
     containerSwitch.addEventListener('click', () => {
         containerSwitch.classList.toggle('dark');
         body.classList.toggle('dark');
         cabecalho.classList.toggle('dark');
         containerBotoes.classList.toggle('dark');
     });
-}
+    try {
+        const response = await fetch(`${API_BASE_URL}/PersonConfigById?PersonId=${infoLS.id}`);
+        if (!response.ok) throw new Error(`Erro ao carregar informações: ${response.status}`);
+        const result = await response.json();
+        if (result.DefaultThemeId === 1) modificaTema();
+      } catch (error) {
+        console.error("Erro ao recuperar tema:", error);
+      }
+    }
+
 
 // Inicialização
 clickTema();
