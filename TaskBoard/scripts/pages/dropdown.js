dfbc33b29e70b3236ccf85ca9646dab4cee2e84d
cecimoura <cecimoura91@gmail.com>
2024-12-04 14:17:42 -0300

    Carregando os boards mas sem o conteudo

--- TaskBoard/scripts/pages/dropdown.js
+++ TaskBoard/scripts/pages/dropdown.js
@@ -35,38 +35,28 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 // Função que preenche o dropdown com os boards recebidos da API
-function populateColumns(data, dropdownContent) {
-  dropdownContent.innerHTML = "";
+async function populateColumns(data, dropdownContent) {
+  dropdownContent.innerHTML = ""; // Limpa o conteúdo do dropdown
 
   data.forEach((board) => {
     const listItem = document.createElement("li");
     const link = document.createElement("a");
     link.className = "dropdown-item";
-    link.innerHTML = board.Name;
-    const boardId = board.Id;
-    carregarColunas(boardId);
+    link.textContent = board.Name;
+    link.href = "#";
+
+    // Evento para carregar as colunas do board selecionado
+    link.addEventListener("click", async () => {
+      localStorage.setItem("currentBoardId", board.Id); // Salva o ID do board no Local Storage
+      await carregarColunas(board.Id); // Carrega as colunas do board selecionado
+    });
 
     listItem.appendChild(link);
     dropdownContent.appendChild(listItem);
   });
 }
 
-async function carregarColunas(boardId) {
-  try {
-    const response = await fetch(
-      `https://personal-ga2xwx9j.outsystemscloud.com/TaskBoard_CS/rest/TaskBoard/ColumnByBoardId?BoardId=${boardId}`
-    );
 
-    if (!response.ok) {
-      throw new Error(`Erro na API: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error("Erro ao carregar as colunas:", error);
-  }
-}
 
 function logout() {
   localStorage.clear();
