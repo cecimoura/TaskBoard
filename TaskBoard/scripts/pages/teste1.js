71c8442624a92edd07fe0a479aaa6af8ddec07ce
cecimoura <cecimoura91@gmail.com>
2024-12-03 21:32:18 -0300

    adicionando aos poucos a função das colunas

---
+++ TaskBoard/scripts/pages/teste1.js
@@ -0,0 +1,10 @@
+
+import { API_BASE_URL } from "../../config/apiConfig.js";
+
+function createColumn(columData){
+    const endpoint = `$ { API_BASE_URL }/Column`;
+}
+
+fetch(endpoint,{
+    method: "POST"
+})
